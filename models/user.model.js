7db711b0d4b9b718807cf894fcfb29586f020b22
Cristian Villafane <cristianv129@hotmail.com>
2020-11-19 20:03:53 -0300

    Agregada id de usuario al modelo

--- models/user.model.js
+++ models/user.model.js
@@ -8,4 +8,10 @@ let schema = new Schema({
     isAdmin: Boolean
 })
 
+schema.method("toJSON", function () {
+    const { __v, _id, ...object } = this.toObject();
+    object.id = _id;
+    return object;
+});
+
 module.exports = mongoose.model("User", schema);
